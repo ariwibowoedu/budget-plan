25ed09eb3372ab8b99f6f8392f18dd667c85a2f0
ari wibowo <ariwibowo.edu@gmail.com>
2025-06-07 11:36:25 +0700

    fixed format currency

--- src/components/utils/formatCurrency.js
+++ src/components/utils/formatCurrency.js
@@ -1,9 +1,14 @@
 export function formatCurrency(value) {
     const amount = Number(value)
     if (isNaN(amount)) return 'Rp 0'
-    return value.toLocaleString('id-ID', {
+
+
+    return new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'IDR',
-        minimumFractionDigits: 0
-    })
+        notation: 'compact',
+        compactDisplay: 'short',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 1,
+    }).format(amount)
 }
